bb26b39fea3170027f0a312305ff7e7d72d5737a
Anderson <andersonlizarazo27@gmail.com>
2020-11-28 18:36:20 -0500

    Añadir compatibilidad al formato PYZ.
    Ahora la variable "BASE_CONFIG" se modifica en tiempo de ejecución para
    adaptarse al formato PYZ.
    
    Las funciones "set_config" y "get_config", ahora aceptan "None" en el
    argumento "file", el comportamiento es el mismo, no se necesita de
    modificaciones para mantener la compatibilidad.
    
    Se añadió la variable "BASE_CONFIG_PYZ" para usar la ruta de archivos
    PYZ.

--- transfer/config.py
+++ transfer/config.py
@@ -14,13 +14,16 @@ Lee la información de un archivo de configuración.
 from json import dump, load
 from os.path import abspath, dirname
 from os.path import join as join_path
-from typing import Dict, Text
+from typing import Dict, Text, Union
 
 ##############################################################################
 ############################### CONSTANTES. ##################################
 ##############################################################################
 
+# Archivo de configuración.
 CONFIG_FILE = join_path(abspath(dirname(__file__)), 'ntp.json')
+# Archivo de configuración para el formato PYZ.
+CONFIG_FILE_PYZ = join_path(dirname(dirname(CONFIG_FILE)), 'ntp.json')
 BASE_CONFIG = {
     'colors': {
         'nickname': 'red',
@@ -38,25 +41,33 @@ BASE_CONFIG = {
 ################################ FUNCIONES. ##################################
 ##############################################################################
 
-def get_config(file: Text = CONFIG_FILE):
+def get_config(file: Union[Text, None] = None):
     """Carga la configuración de un archivo.
     
     :file: Es la ruta donde se buscará la configuración.
+
+    Si "file" es "None" se utilizará el valor de la variable "CONFIG_FILE".
     """
+
+    if file is None: file = CONFIG_FILE
 
     with open(file, 'rt') as fp:
         file = load(fp)
 
     return file
 
-def set_config(config: Dict[Dict, Dict], file: Text = CONFIG_FILE):
+def set_config(config: Dict[Dict, Dict], file: Union[Text, None] = None):
     """Establece la configuración de un archivo.
     
     :file:   Es la ruta donde se guardará la configuración.
     :config: Debe ser un diccionario conteniendo otros dos diccionarios, cada 
              uno contiene un aspecto de configuración de NTP (Ver la constante 
              "CONFIG_FILE").
+
+    Si "file" es "None" se utilizará el valor de la variable "CONFIG_FILE".
     """
+
+    if file is None: file = CONFIG_FILE
 
     with open(file, 'wt') as fp:
         dump(
@@ -73,8 +84,13 @@ def set_config(config: Dict[Dict, Dict], file: Text = CONFIG_FILE):
 ##############################################################################
 
 try: get_config()
-except FileNotFoundError: set_config(BASE_CONFIG)
-except OSError: 
+except FileNotFoundError: 
+    try: set_config(BASE_CONFIG)
+    except FileNotFoundError:
+        CONFIG_FILE = CONFIG_FILE_PYZ # Usar rutas para PYZ.
+        try: get_config()
+        except FileNotFoundError: set_config(BASE_CONFIG)
+except OSError:
     print(
         'Verifique que no halla ningún achivo o carpeta llamada:' \
         f'"{CONFIG_FILE}".',
